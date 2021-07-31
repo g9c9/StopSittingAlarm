41c284895a5a67e31df80936b060a08fdc812381
George <g9c9@outlook.com>
2021-07-30 22:19:32 -0700

    Updated all components to use ES6 classes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
+import React from "react";
 import Clock from "./Clock";
 
-function App() {
-  return (
-    <div>
-      <h1>Stop Sitting</h1>
-      <Clock />
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Stop Sitting</h1>
+        <Clock />
+      </div>
+    );
+  }
 }
 
 export default App;
