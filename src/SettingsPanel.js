28dc8b685394b82a211b8cced9c79b3667f57ae8
George <g9c9@outlook.com>
2021-08-31 20:59:43 -0700

    Added basic form to SettingsPanel

--- src/SettingsPanel.js
+++ src/SettingsPanel.js
@@ -1,5 +1,14 @@
 function SettingsPanel(props) {
-  return <h1>Settings Panel</h1>;
+  return (
+    <>
+      <h1>Settings Panel</h1>
+      <form>
+        <label>Sitting Time</label>
+        <br />
+        <label>Break Time</label>
+      </form>
+    </>
+  );
 }
 
 export default SettingsPanel;
