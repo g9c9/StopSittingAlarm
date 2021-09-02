a0224908f2ac453db244edb37ab936ee467284d1
George <g9c9@outlook.com>
2021-09-01 20:51:58 -0700

    Fixed bug

--- src/Timer.js
+++ src/Timer.js
@@ -29,7 +29,7 @@ function useTimer(timeLimit, timeUp) {
   }, [startTimer]);
 
   useEffect(() => {
-    if (min === timeLimit) {
+    if (min >= timeLimit) {
       timeUp();
       setStartTimer(false);
     }
