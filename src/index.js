b94ffbeb439eea46eb48d6f22705649f3128c354
George <g9c9@outlook.com>
2021-10-04 20:44:06 -0700

    Added basic desktop notification and index.css

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./index.css";
 import App from "./components/App";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
@@ -9,3 +10,15 @@ ReactDOM.render(<App />, document.getElementById("root"));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
 serviceWorkerRegistration.register();
+
+(async function () {
+  // Asking user for notification permission
+  await Notification.requestPermission();
+
+  // Send greeting if permisison is granted
+  if (Notification.permission === "granted") {
+    const greeting = new Notification("Hi, How are you?");
+    //Close greeting in 5 seconds
+    setTimeout(() => greeting.close(), 5 * 1000);
+  }
+})();
