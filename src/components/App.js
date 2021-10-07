caefaac2f9a58e8a0acc9921fbcafc615260c13b
George <g9c9@outlook.com>
2021-10-06 23:49:43 -0700

    Asks notif permission if enableNotif is set to true

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,23 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Greeting from "./Greeting";
 import Clock from "./Clock";
 import StopSittingPanel from "./StopSittingPanel";
 
 function App() {
   const [enableNotif, setEnableNotif] = useState(false);
+
+  useEffect(() => {
+    async function getPermission() {
+      if (
+        enableNotif &&
+        Notification.permission !== "granted" &&
+        Notification.permission !== "denied"
+      ) {
+        await Notification.requestPermission();
+      }
+    }
+    getPermission();
+  }, [enableNotif]);
 
   return (
     <>
